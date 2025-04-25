8f4762d06cb6ac220b676aefdf5f0d7111e08d7e
Connor Heath <ctheath09gmail.com>
2025-04-24 20:42:37 -0400

    fix: added a = to the if

--- format_changer.js
+++ format_changer.js
@@ -1,13 +1,10 @@
 const formatChanger = document.querySelector("button[id='format_button']");
 
 formatChanger.addEventListener("click",(event)=>{
-    let formatValue = document.querySelector("select[id='format_select']").value;
-    console.log(formatValue)
-    if (formatValue = "1") {
-        document.querySelector("link").setAttribute("href","");
-        console.log("night");
-    }else if(formatValue = "0"){
+    let formatValue = document.querySelector("select[name='format_changer']").value;
+    if (formatValue == "night") {
+        document.querySelector("link").setAttribute("href","format1.css");
+    }else if(formatValue == "none"){
         document.querySelector("link").setAttribute("href","none.css");
-        console.log("0");
     };
 });
