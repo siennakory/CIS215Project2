0d28c5da43426844ecee39091d7922bb95a996f5
siennakory <siennakory@gmail.com>
2025-04-27 22:10:53 -0400

    refactor: removed testing console.log commands

--- charcount.js
+++ charcount.js
@@ -3,11 +3,8 @@ const divcount = document.querySelector("div[id='char-count']")
 
 function checkCount(event){
     let currentText = event.target.value;
-    console.log(currentText);
     let textBoxCount = currentText.length;
-    console.log(textBoxCount);
     let charCount = (120 - textBoxCount);
-    console.log(charCount);
 
     if (textBoxCount > 70){
         divcount.textContent = charCount;
